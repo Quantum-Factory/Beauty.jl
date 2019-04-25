e49e2337de7459d22e3feb2f1cf02ea5502acbf0
Bjoern Stein <bjoern.stein@quantum-factory.de>
2019-04-25 11:55:31 +0200

    fix doc build and change doc links to github repo

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,5 @@ using Beauty
 makedocs(
     sitename="quantum-factory.de",
     format = Documenter.HTML(prettyurls = false),
-    modules = [Beauty],
-    repo = string(
-        "https://bitbucket.org/quantumfactory/beauty.jl",
-        "/src/{commit}{path}#lines-{line}"
-    )
+    modules = [Beauty]
 )
